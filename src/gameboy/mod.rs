1ff04b8bb22de85aa21f93a81522dde4dec4b34f
Tyler Johnson <Tylermackj@gmail.com>
2021-06-01 17:35:28 -0500

    Added more instructions. Needs error checking.

--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -5,14 +5,14 @@ mod registers;
 use registers::*;
 
 pub struct Gameboy {
-    memory: [u8; 0xFFFF],
+    memory: [u8; 0x10000],
     pub registers: Registers,
 }
 
 impl Gameboy {
     pub fn new() -> Gameboy {
         Gameboy {
-            memory: [0u8; 0xFFFF],
+            memory: [0u8; 0x10000],
             registers: Registers::new(),
         }
     }
@@ -66,9 +66,38 @@ impl Gameboy {
         match instruction {
             0x00 => {},
 
-            0x28 => self.jr(Flag::Z, true),
+            0x18 => self.jr(true),
+
+            0x28 => self.jr(self.registers.get_flag(Flag::Z) == true),
+
+            0x3e => {
+                let d8: u8 = self.get_at_pc_incr();
+                self.registers.set_a(d8);
+            }
 
             0xc3 => self.jmp(),
+
+            0xaf => {
+                let a: u8 = self.registers.get_a();
+                self.xor(a);
+            }
+
+            0xe0 => {
+                // (0xFF00 + n) <= A
+                let offset = self.get_at_pc_incr();
+                let a: u8 = self.registers.get_a();
+                self.memory[0xFF00 + offset as usize] = a;
+            }
+
+            0xea => {
+                // (nn) <= A
+                // ROM CHECK
+                let addr: u16 = self.get_at_pc_incr() as u16 | ((self.get_at_pc_incr() as u16) << 8);
+                let a: u8 = self.registers.get_a();
+                self.memory[addr as usize] = a;
+            }
+
+            0xf3 => self.interrupts_enabled(false),
 
             0xfe => {
                 let d8: u8 = self.get_at_pc_incr();
@@ -95,25 +124,15 @@ impl Gameboy {
     pub fn cp(&mut self, n: u8) {
         let a: u8 = self.registers.get_a();
 
-        if a == n {
-            self.registers.set_flag(Flag::Z, true);
-        } else {
-            self.registers.set_flag(Flag::Z, false);
-        }
-
+        self.registers.set_flag(Flag::Z, a == n);
         self.registers.set_flag(Flag::N, true);
+        self.registers.set_flag(Flag::H, (a & 0x0F).checked_sub(n & 0x0F) == None);
+        self.registers.set_flag(Flag::C, a < n);
+    }
 
-        if (a & 0x0F).checked_sub(n & 0x0F) == None {
-            self.registers.set_flag(Flag::H, true);
-        } else {
-            self.registers.set_flag(Flag::H, false);
-        }
-
-        if a < n {
-            self.registers.set_flag(Flag::C, true);
-        } else {
-            self.registers.set_flag(Flag::C, false);
-        }
+    // Enable and disable interrupts
+    pub fn interrupts_enabled(&mut self, enabled: bool) {
+        self.memory[0xffff] = enabled as u8;
     }
 
     // Jump Unconditional
@@ -123,16 +142,22 @@ impl Gameboy {
     }
 
     // Jump Relative Conditional
-    pub fn jr(&mut self, flag: Flag, set: bool) {
+    pub fn jr(&mut self, condition: bool) {
         let offset: u8 = self.get_at_pc_incr();
-        let mut jump: bool = false;
 
-        if self.registers.get_flag(flag) == set {
-            jump = true;
-        }
-
-        if jump {
+        if condition {
             self.registers.set_pc(self.registers.get_pc() + offset as u16 - 1);  // -1 added for pc change to get relative values
         }
+    }
+
+    // XOR n with A => A
+    pub fn xor(&mut self, n: u8) {
+        let value: u8 = self.registers.get_a() ^ n;
+        self.registers.set_a(value);
+
+        self.registers.set_flag(Flag::Z, value == 0);
+        self.registers.set_flag(Flag::N, false);
+        self.registers.set_flag(Flag::H, false);
+        self.registers.set_flag(Flag::C, false);
     }
 }
